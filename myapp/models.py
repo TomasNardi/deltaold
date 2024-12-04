e1012cb8cc9c8200815cea85a8895232655b1de9
Tomas Nardi <tomas.nardi@hotmail.com>
2024-12-04 16:01:25 -0300

    test

--- myapp/models.py
+++ myapp/models.py
@@ -17,7 +17,7 @@ class Task(models.Model):
     done = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     fecha = models.DateTimeField(null=True)
-    creado = models.DateTimeField(auto_now_add=False)
+    creado = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return (
--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -14,16 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
-
-def create_superuser():
-    username = "deltaoldadmin"
-    email = "admin@example.com"
-    password = "chile1996"
-
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username=username, email=email, password=password)
-        print("Superusuario creado exitosamente.")
-    else:
-        print("El superusuario ya existe.")
-
-create_superuser()
