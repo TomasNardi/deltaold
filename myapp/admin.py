a3886289f200056a2600a7b2979b575036606dbc
Tomas Nardi <tomas.nardi@hotmail.com>
2024-12-06 18:13:37 -0300

    test

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
-from .models import Productos , Eventos
+from .models import Productos, Eventos, EstadoCarta
 
-    
+
 class ProductosAdmin(admin.ModelAdmin):
     pass
+
 
 class EventosAdmin(admin.ModelAdmin):
     pass
 
-# Register your models here.
-admin.site.register(Productos ,ProductosAdmin)
-admin.site.register(Eventos ,EventosAdmin)
 
+class EstadoCartaAdmin(admin.ModelAdmin):
+    pass
+
+
+# Register your models here.
+admin.site.register(Productos, ProductosAdmin)
+admin.site.register(Eventos, EventosAdmin)
+admin.site.register(EstadoCarta, EstadoCartaAdmin)
